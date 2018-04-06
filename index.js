b21b99714160684f60d93465c935cdda2135fc85
Shinichi Kogai <kogai0121@gmail.com>
2018-04-06 23:25:11 +0900

    tackle gql

--- index.js
+++ index.js
@@ -1,52 +1,56 @@
+const fs = require("fs");
 const fetch = require("node-fetch");
 
-const fetchQuery = (
-  query,
-  variables,
-) => {
-  return fetch('https://api.github.com/graphql', {
-    method: 'POST',
+const fetchQuery = (query, variables) => {
+  return fetch("https://api.github.com/graphql", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `bearer ${process.env.GITHUB_API_TOKEN}`
+      "Content-Type": "application/json",
+      Authorization: `bearer ${process.env.GITHUB_API_TOKEN}`
     },
     body: JSON.stringify({
       query,
-      variables,
-    }),
-  }).then(response => {
-    return response.json();
-  }).catch(e => {
-    console.error(e);
-  });
-}
+      variables
+    })
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(e => {
+      console.error(e);
+    });
+};
 
-fetchQuery(`
-{
-  repository(owner:"kogai", name:"typed_i18n") {
-    pullRequests(last: 10) {
-      edges {
-        node {
-          title
-          createdAt
-          closed
-          author {
-            avatarUrl
-            url
-          }
-        }
+const fromDay = new Date("2018-03-01");
+const toDay = new Date("2018-03-31");
+console.log(fromDay, toDay);
+
+const query = fs.readFileSync("./Query.gql").toString();
+
+let count = 0;
+const fetchAll = (xs = [], cursor) => {
+  return fetchQuery(query, {
+    login: "kogai",
+    last: 5,
+    before: cursor
+  }).then(
+    ({
+      data: {
+        user: { pullRequests: { pageInfo: { endCursor, startCursor }, edges } }
       }
-    }
-  }
-}
-`).then(({
-  data: {
-    repository: {
-      pullRequests: {
-        edges
+    }) => {
+      const ys = edges
+        .map(({ node: { title, createdAt } }) => `${createdAt}:${title}`)
+        .concat(xs);
+      if (count > 2) {
+        return Promise.resolve(ys);
       }
+      count++;
+      return fetchAll(ys, startCursor);
     }
-  }
-}) => {
-  console.log(edges);
-})
+  );
+};
+
+fetchAll().then(xs => {
+  console.log(xs);
+});
