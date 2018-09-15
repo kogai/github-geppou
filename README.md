d063ee4134c4a794ad62ab1e85460ae692ecc171
Shinichi Kogai <kogai@users.noreply.github.com>
2018-09-15 12:54:11 +0900

    Make the tool more reasonable (#3)
    * :fire: Use npm instead of yarn
    
    * :fire: Remove confusable unused files
    
    * :sparkles: Use arguments instead of embedded one
    
    * Update Readme aimed to more usable

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 Yet another GitHub-nippou
 
-* https://github.com/masutaka/github-nippou
-* https://github.com/zenoplex/github-summary
+- https://github.com/masutaka/github-nippou
+- https://github.com/zenoplex/github-summary
 
-## Need
+## Usage
 
-* GITHUB_API_TOKEN
-* pbcopy
-* xsel
-* win-clipboard // https://github.com/sindresorhus/win-clipboard
+1.  Set `GITHUB_API_TOKEN` into environment variables.
+1.  Install CLI which can help to use your clipboard, Either pbcopy(MacOS), xsel(Linux), win-clpboard(Windows)
+1.  Run `npm start your-github-username, from-day to-day`
+
+```sh
+# Example
+npm start kogai 2018-08-01 2018-08-31
+```
--- index.js
+++ index.js
@@ -3,18 +3,26 @@ const fetch = require("node-fetch");
 const clipoardy = require("clipboardy");
 const im = require("immutable");
 
+const [_, __, user, _fromDay, _toDay] = process.argv;
+if (!(user && _fromDay && _toDay)) {
+  console.error(`Expected three parameters.
+npm start [username] [fromDay: yyyy-MM-DD] [toDay: yyyy-MM-DD]
+`);
+  process.exit(1);
+}
+
 const fetchQuery = (query, variables) => {
   return fetch("https://api.github.com/graphql", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `bearer ${process.env.GITHUB_API_TOKEN}`
-      },
-      body: JSON.stringify({
-        query,
-        variables
-      })
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `bearer ${process.env.GITHUB_API_TOKEN}`
+    },
+    body: JSON.stringify({
+      query,
+      variables
     })
+  })
     .then(response => {
       return response.json();
     })
@@ -23,19 +31,18 @@ const fetchQuery = (query, variables) => {
     });
 };
 
-const isUptoDate = (xs, edges) => xs.length === 0 &&
-  edges.every(({
-    node: {
-      createdAt
-    }
-  }) => fromDay.getTime() >= new Date(createdAt).getTime());
+const isUpToDate = (xs, edges) =>
+  xs.length === 0 &&
+  edges.every(
+    ({ node: { createdAt } }) =>
+      fromDay.getTime() >= new Date(createdAt).getTime()
+  );
 
-const fromDay = new Date("2018-06-01");
-const toDay = new Date("2018-06-31");
+const fromDay = new Date(_fromDay);
+const toDay = new Date(_toDay);
 
 const pullRequest = fs.readFileSync("./PullRequest.gql").toString();
 const repository = fs.readFileSync("./Repository.gql").toString();
-const user = "kogai";
 
 const fetchPullRequests = (query, xs = [], cursor = undefined) => {
   return fetchQuery(query, {
@@ -47,35 +54,20 @@ const fetchPullRequests = (query, xs = [], cursor = undefined) => {
       data: {
         user: {
           pullRequests: {
-            pageInfo: {
-              endCursor,
-              startCursor
-            },
+            pageInfo: { endCursor, startCursor },
             edges
           }
         }
       }
     }) => {
       const ys = edges
-        .filter(({
-          node: {
-            createdAt
-          }
-        }) => {
+        .filter(({ node: { createdAt } }) => {
           const d = new Date(createdAt);
           return (
             fromDay.getTime() <= d.getTime() && toDay.getTime() >= d.getTime()
           );
         })
-        .map(({
-          node: {
-            title,
-            createdAt,
-            url,
-            author,
-            repository
-          }
-        }) => {
+        .map(({ node: { title, createdAt, url, author, repository } }) => {
           return {
             title,
             createdAt,
@@ -84,7 +76,7 @@ const fetchPullRequests = (query, xs = [], cursor = undefined) => {
             repository
           };
         });
-      if (isUptoDate(ys, edges)) {
+      if (isUpToDate(ys, edges)) {
         return Promise.resolve(ys.concat(xs));
       }
       console.log(
@@ -106,46 +98,27 @@ const fetchRepositories = (query, xs = [], cursor = undefined) =>
       data: {
         user: {
           repositories: {
-            pageInfo: {
-              endCursor,
-              startCursor
-            },
+            pageInfo: { endCursor, startCursor },
             edges
           }
         }
       }
     }) => {
       const ys = edges
-        .filter(({
-          node: {
-            isFork,
-            isPrivate
-          }
-        }) => !isFork && !isPrivate)
-        .filter(({
-          node: {
-            createdAt
-          }
-        }) => {
+        .filter(({ node: { isFork, isPrivate } }) => !isFork && !isPrivate)
+        .filter(({ node: { createdAt } }) => {
           const d = new Date(createdAt);
           return (
             fromDay.getTime() <= d.getTime() && toDay.getTime() >= d.getTime()
           );
         })
-        .map(({
-          node: {
-            name,
-            createdAt,
-            owner,
-            url
-          }
-        }) => ({
+        .map(({ node: { name, createdAt, owner, url } }) => ({
           name,
           createdAt,
           owner,
           url
         }));
-      if (isUptoDate(ys, edges)) {
+      if (isUpToDate(ys, edges)) {
         return Promise.resolve(ys.concat(xs));
       }
       console.log(
@@ -161,20 +134,13 @@ const groupRequest = xs =>
     ({
       repository: {
         name,
-        owner: {
-          login
-        }
+        owner: { login }
       }
     }) => `${login}/${name}`
   );
 
 const groupRepository = xs =>
-  im.List(xs).groupBy(({
-    name,
-    owner: {
-      login
-    }
-  }) => `${login}/${name}`);
+  im.List(xs).groupBy(({ name, owner: { login } }) => `${login}/${name}`);
 
 const formatElement = ({
   title,
@@ -182,7 +148,6 @@ const formatElement = ({
   url,
   author,
   repository,
-
   name,
   owner
 }) => {
@@ -197,10 +162,10 @@ const formatElement = ({
 Promise.all([
   fetchPullRequests(pullRequest),
   fetchRepositories(repository)
-]).then(([prs, repos]) => {
+]).then(([prs, repositories]) => {
   const result = groupRequest(prs)
-    .mergeDeep(groupRepository(repos))
-    .map((ys) => ys.map(formatElement))
+    .mergeDeep(groupRepository(repositories))
+    .map(ys => ys.map(formatElement))
     .map(ys => ys.join("\n"))
     .reduce((acc, v, k) => acc.push(`### ${k}\n\n${v}`), im.List())
     .join("\n\n\n");
